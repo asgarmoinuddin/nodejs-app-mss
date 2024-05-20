23b76c43ff14415257693bb16951c6901d9d997c
asgarmoinuddin <132954400+asgarmoinuddin@users.noreply.github.com>
2024-05-20 14:54:45 -0400

    Update sonar-project.js

--- sonar-project.js
+++ sonar-project.js
@@ -1,12 +1,12 @@
 const sonarqubeScanner = require('sonarqube-scanner');
 
 sonarqubeScanner({
-  serverUrl: 'http://13.234.231.110:9000/',
+  serverUrl: 'http://172.31.39.75:9000/',
        options : {
 	    'sonar.projectDescription': 'This is a Node JS application',
 	    'sonar.projectName': 'Node JS Application - Sample',
 	    'sonar.projectKey':'NodeJsMithunTechnologies',
-	    'sonar.login': 'squ_2fa527428b070f0ebb506d109baf7062b915693a',
+	    'sonar.login': 'squ_8bfff627d6c8995ef897a082ce74db09485922fb',
 	    //'sonar.login': 'admin',
 	    //'sonar.password': 'admin',
             'sonar.projectVersion':'1.0',
